a0e8eb53c6314232d42c479b80b66ba6facbf057
Maxim <info@wi-fi-point.com>
2020-03-10 21:26:59 +0200

    Create SmsRuMessage.php

---
+++ src/SmsRuMessage.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Kagatan\SmsRu;
+
+
+class SmsRuMessage
+{
+    /**
+     * API Key
+     *
+     * @var
+     */
+    public $key;
+
+    
+    /**
+     * The text content of the message.
+     *
+     * @var string|null
+     */
+    public $text;
+
+
+    /**
+     * Receiver phone number.
+     *
+     * @var string|null
+     */
+    public $to;
+
+
+    /**
+     * Sender name. Leave 'null' for using value from settings.
+     *
+     * @var string|null
+     */
+    public $from;
+
+
+    /**
+     * Static factory method.
+     *
+     * @param mixed ...$arguments
+     *
+     * @return static|self
+     */
+    public static function create(...$arguments)
+    {
+        return new static(...$arguments);
+    }
+
+
+    /**
+     * Set key
+     *
+     * @param $key
+     * @return $this
+     */
+    public function key($key)
+    {
+        $this->key = (string)$key;
+        return $this;
+    }
+
+
+    /**
+     * Set a sender name.
+     *
+     * @param string $sender_name
+     *
+     * @return static|self
+     */
+    public function from($sender_name)
+    {
+        $this->from = (string)$sender_name;
+        return $this;
+    }
+
+    /**
+     * Set receiver phone number (the message should be sent to).
+     *
+     * @param string $phone_number
+     *
+     * @return static|self
+     */
+    public function to($phone_number)
+    {
+        $this->to = (string)$phone_number;
+        return $this;
+    }
+
+    /**
+     * Set the content of SMS message.
+     *
+     * @param string $content
+     *
+     * @return static|self
+     */
+    public function content($content)
+    {
+        $this->text = (string)$content;
+        return $this;
+    }
+
+
+    /**
+     *  To JSON
+     *
+     * @param int $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
+    }
+
+
+    /**
+     * To Array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return (array)$this;
+    }
+}
